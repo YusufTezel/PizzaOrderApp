62dfffd450548c414aad4d279962fa839a8ca712
Yusuf Tezel <tezelyusuf@gmail.com>
2017-12-29 01:03:48 +0100

    Startup files added, ink pods.

---
+++ PizzaOrderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PizzaOrderApp
+//
+//  Created by Yusuf Tezel on 29/12/2017.
+//  Copyright © 2017 tezel. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+
